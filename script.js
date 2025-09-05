75a658d06f653f0d146f3792cfb71272f63d927a
Aryan singh <140242916+aryansingh186@users.noreply.github.com>
2025-09-05 14:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const textEl = document.querySelector('.text p');
+// const content = textEl.innerText;
+// textEl.innerHTML = "";
+
+// // calculate step angle based on text length
+// const angleStep = 360 / content.length;
+
+// textEl.innerHTML = content
+//   .split("")
+//   .map(
+//     (char, i) =>
+//       `<span style="transform: rotate(${i * angleStep}deg) translate(100px) rotate(${-(i * angleStep)}deg)">${char}</span>`
+//   )
+//   .join("");
